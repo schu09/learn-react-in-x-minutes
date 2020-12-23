fb3ac1262da814de6866497cc10deaa70e352cd0
Sebastian Chu <chusebatian@gmail.com>
2020-12-23 05:24:50 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
-    const [todos, setTodos] = useState([
-        { id: 1, name: "Todo 1", complete: true },
-    ]); // Object Destructuring
+    const [todos, setTodos] = useState([]); // Object Destructuring
+    const todoNameRef = useRef();
+
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (storedTodos) setTodos(storedTodos);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    }, [todos]);
+
+    function toggleTodo(id) {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id === id);
+        todo.complete = !todo.complete;
+        setTodos(newTodos);
+    }
+
+    function handleAddTodo(e) {
+        const name = todoNameRef.current.value;
+        if (name === "") return;
+        setTodos((prevTodos) => {
+            return [
+                ...prevTodos,
+                { id: uuidv4(), name: name, complete: false },
+            ];
+        });
+        todoNameRef.current.value = null;
+    }
+
     return (
         <>
-            <TodoList todos={todos} />
-            <input type="text" />
-            <button>Add Todo</button>
+            <TodoList todos={todos} toggleTodo={toggleTodo} />
+            <input ref={todoNameRef} type="text" />
+            <button onClick={handleAddTodo}>Add Todo</button>
             <button>Clear completed</button>
             <div>0 left to do</div>
         </>

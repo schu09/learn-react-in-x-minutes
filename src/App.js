164f19cafb3d25e92f46129e60225ff5efaff362
Sebastian Chu <chusebatian@gmail.com>
2020-12-23 04:39:00 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+
 function App() {
-    return null;
+    const [todos, setTodos] = useState(["Todo 1", "Todo 2"]); // Object Destructuring
+    return (
+        <>
+            <TodoList todos={todos} />
+            <input type="text" />
+            <button>Add Todo</button>
+            <button>Clear completed</button>
+            <div>0 left to do</div>
+        </>
+    );
 }
 
 export default App;
